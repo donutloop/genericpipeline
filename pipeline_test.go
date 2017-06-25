978a10946c0a1f5dee29d6a33f49dc1868be3813
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2017-06-25 12:37:32 +0200

    - Updated: Support of multi reuse added

--- pipeline_test.go
+++ pipeline_test.go
@@ -26,7 +26,7 @@ func TestPipeline(t *testing.T) {
 		return n * n, nil
 	}
 
-	p, err := Create(fn, fn1, fn2)
+	p, err := Create(1000, fn, fn1, fn2)
 	if err != nil {
 		t.Error(err)
 		return
@@ -75,7 +75,7 @@ func TestPipelineWithContext(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
-	p, err := CreateWithContext(ctx, fn, fn1, fn2)
+	p, err := CreateWithContext(ctx, 1000, fn, fn1, fn2)
 	if err != nil {
 		t.Error(err)
 		return
@@ -86,5 +86,52 @@ func TestPipelineWithContext(t *testing.T) {
 	if err == nil || err.Error() != "context deadline exceeded" {
 		t.Error(err)
 		return
+	}
+}
+
+func TestPipelineManyValues(t *testing.T) {
+
+	fn := func(s string) (string, error) {
+		return s + s, nil
+	}
+
+	fn1 := func(s string) (int, error) {
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return -1, err
+		}
+
+		return i, err
+	}
+
+	fn2 := func(n int) (int, error) {
+		return n * n, nil
+	}
+
+	p, err := Create(1000, fn, fn1, fn2)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	for i := 0; i < 1000; i++ {
+		p.Input("42")
+		vRaw, err := p.Output()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		v, ok := vRaw.(int)
+		if !ok {
+			t.Error("Unexpected kind of value")
+			return
+		}
+
+		ExpectedValue := 17994564
+		if v != ExpectedValue {
+			t.Errorf("Unexpected value (Actual: %d, Expected: %d)", v, ExpectedValue)
+			return
+		}
 	}
 }
